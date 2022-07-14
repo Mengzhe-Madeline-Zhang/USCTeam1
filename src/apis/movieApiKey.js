d033efcd12acaf427341e8bd32af2aec79e54020
DaLaoYY <109172415+DaLaoYY@users.noreply.github.com>
2022-07-14 19:27:45 -0400

    Delete src directory

--- src/apis/movieApiKey.js
+++
@@ -1,2 +0,0 @@
-export const APIkey = "3e7ad68d5fd97e7c08a68152a0870544";
-
--- src/features/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import moviesReducer from "../compoent/Slice"
-
-export const store = configureStore({
-    reducer: {
-        movies: moviesReducer,
-    }
-});
-
