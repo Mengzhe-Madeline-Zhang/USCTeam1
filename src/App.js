50d49d3c29777ca5d23cad7f3b5683055936d8b2
DaLaoYY <109172415+DaLaoYY@users.noreply.github.com>
2022-07-14 17:36:22 -0400

    Add files via upload
    To solve ticket # 3, fetch data from api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MovieList from './compoent/MovieList';
+import Pagination from "./compoent/Pagination";
+
+
+function App() {
+  return (
+    <div className="Todo-App">
+        <MovieList/>
+    </div>
+  );
+}
+
+export default App;
